7871b5ee4487e9a61867c61108dd6d5873968312
deborahregina <deborahreginasilveira@gmail.com>
2022-08-24 15:46:30 -0300

    message: primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <main class="columns is-gapless is-multiline"> 
+  <main class="columns is-gapless is-multiline" :class="{'modo-escuro': modoEscuroAtivo}"> 
   <div class = "column is-one-quarter">
-  <BarraLateral />
+  <BarraLateral @aoTemaAlterado="trocarTema"/>
   </div>
-  <div class="column is-three-quarter">
-    <Formulario />
+  <div class="column is-three-quarter conteudo">
+    <Formulario @aoSalvarTarefa="salvarTarefa"/>
+    <div class="lista">
+      <Tarefa v-for="tarefa,index in tarefas" :key ="index" :tarefa="tarefa"/>
 
+    </div>
 
   </div>
 
@@ -17,16 +20,48 @@
 import { defineComponent } from 'vue';
 import BarraLateral from './components/BarraLateral.vue';
 import Formulario from './components/Formulario.vue';
+import Tarefa from './components/Tarefa.vue';
+import ITarefa from './interfaces/ITarefa';
 export default defineComponent({
   name: 'App',
   components: {
     BarraLateral,
-    Formulario
+    Formulario,
+    Tarefa
+}, 
+data () {
+  return {
+    tarefas: [] as ITarefa[],
+    modoEscuroAtivo: false
+  }
+},
+methods : {
+  salvarTarefa (tarefa: ITarefa) {
+    this.tarefas.push(tarefa)
+  },
+  trocarTema(modoEscuroAtivo: boolean) {
+    this.modoEscuroAtivo = modoEscuroAtivo
+  }
 }
  
 });
 </script>
 
 <style>
+.lista {
+  padding: 1.25
+}
 
+main {
+  --bg-primario: #fff;
+  --text-primario: #000;
+}
+
+main.modo-escuro {
+  --bg-primario: #2b2d42;
+  --text-primario: #ddd;
+}
+.conteudo {
+  background-color: var(--bg-primario);
+}
 </style>
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="box">
+    <div class="box formulario">
         <div class="columns">
             <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
-                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
+                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?"
+                v-model="descricao">
             </div>
             <div class="column">
-                <Temporizador />
+                <Temporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
             </div>
         </div>
     </div>
@@ -17,8 +18,30 @@ import Temporizador from "./Temporizador.vue";
 
 export default defineComponent({
     name: "FormularioTarefa",
+    emits: ['aoSalvarTarefa'],
     components: {
         Temporizador
+    },
+    data() {
+        return {
+            descricao: ''
+        }
+    },
+    methods: {
+        finalizarTarefa(tempoDecorrido: number) : void {
+            this.$emit('aoSalvarTarefa', {
+                duracaoEmSegundos: tempoDecorrido,
+                descricao: this.descricao
+                })
+        }
     }
 })
 </script>
+
+<style>
+.formulario {
+    color: var(--text-primario);
+    background-color: var(--bg-primario);
+}
+
+</style>
